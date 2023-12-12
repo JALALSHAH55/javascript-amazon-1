04a60673b8677fdcc9fb0e2446ccce0c4974bef0
jalalshah <jalal.shah125125@gmail.com>
2023-12-12 23:23:10 +0500

    delete products

--- script/checkout.js
+++ script/checkout.js
@@ -1,5 +1,6 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML ='';
 
@@ -28,7 +29,7 @@ let cartSummaryHTML ='';
       ${matchingProduct.name}
     </div>
     <div class="product-price">
-      $${(matchingProduct.priceCents / 100).toFixed(2)}
+      $${formatCurrency(matchingProduct.priceCents)}
     </div>
     <div class="product-quantity">
       <span>
@@ -37,7 +38,8 @@ let cartSummaryHTML ='';
       <span class="update-quantity-link link-primary">
         Update
       </span>
-      <span class="delete-quantity-link link-primary">
+      <span class="delete-quantity-link link-primary js-delete-link"
+      data-product-id="${matchingProduct.id}">
         Delete
       </span>
     </div>
@@ -50,7 +52,7 @@ let cartSummaryHTML ='';
     <div class="delivery-option">
       <input type="radio" checked
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
       <div>
         <div class="delivery-option-date">
           Tuesday, June 21
@@ -63,7 +65,7 @@ let cartSummaryHTML ='';
     <div class="delivery-option">
       <input type="radio"
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
       <div>
         <div class="delivery-option-date">
           Wednesday, June 15
@@ -76,7 +78,7 @@ let cartSummaryHTML ='';
     <div class="delivery-option">
       <input type="radio"
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
       <div>
         <div class="delivery-option-date">
           Monday, June 13
@@ -88,8 +90,16 @@ let cartSummaryHTML ='';
     </div>
   </div>
 </div>
-</div>
-`;
+</div>`;
 });
 
  document.querySelector('.js-order-summary').innerHTML= cartSummaryHTML;
+  
+document.querySelectorAll('.js-delete-link')
+  .forEach((link)=>{
+    link.addEventListener('click', ()=>{
+        const productId =link.dataset.productId;
+        removeFromCart(productId);
+      });
+    });
+  
